997d090bee1edec52b216047e60ddf441519fb33
Jeremy <markjeremyj@gmail.com>
2025-06-05 08:38:25 +0300

    adds functions

--- test/indexTest.js
+++ test/indexTest.js
@@ -33,7 +33,7 @@ describe("index.js", () => {
     it("function exists", function() {
       expect(wrapAdjective).to.exist
     })
-    
+
     it("when initialized with '*' creates a function that, when called, wraps an adjective in a highlight", function() {
       let result = wrapAdjective('*')
       let emphatic = result("a hard worker")
